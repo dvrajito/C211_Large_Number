873d47d7038a4baff6e6e424273851a2487dc54b
katiedelucio <125385457+katiedelucio@users.noreply.github.com>
2023-10-29 15:50:23 -0400

    Update TestNumber.java

--- src/phase2/TestNumber.java
+++ src/phase2/TestNumber.java
@@ -1,7 +1,7 @@
 /************************************************************
  * C211 Fall 2023
  * Project Phase 2
- * Authors: Dana Vrajitoru, Gavin Power, Brandon Jones, Charlie Kinnett, Tiffany Leister, Katie DeLucio,
+ * Authors: Dana Vrajitoru, Gavin Power, Brandon Jones, Charlie Kinnett, Tiffany Leister, Katie DeLucio, Madeline Abbott,
  * Class: LargeNumber
  * A test class for the class LargeNumber.
  ************************************************************/
