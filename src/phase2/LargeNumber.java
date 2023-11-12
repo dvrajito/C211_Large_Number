83b7a4ffffc182fc4e11dd1feaad0a94f354bfa0
dvrajito <dvrajito@sbcglobal.net>
2023-11-12 16:26:19 -0500

    Adding back some lost code from SubtractFromLarge

--- src/phase2/LargeNumber.java
+++ src/phase2/LargeNumber.java
@@ -292,6 +292,7 @@ public class LargeNumber implements Comparable<LargeNumber> {
     // LargeNumbers.
     private void subtractFromLarge(ArrayList<Integer> from, ArrayList<Integer> value) {
         int carry = 0;
+        int leadingZeros = 0;
         // return the max number of digit btw this and Other
         int maxDigits = Math.max(from.size(), value.size());
 
@@ -308,11 +309,22 @@ public class LargeNumber implements Comparable<LargeNumber> {
                 carry = 0;
             }
 
+            if (result != 0) {
+                leadingZeros = 0;
+            } else { // this test is not necessary: if (result == 0 && leadingZeros >= 0) {
+                leadingZeros++;
+            }
+
             if (i < from.size()) {
                 from.set(i, result);
             } else {
                 from.add(result);
             }
+        }
+        // Remove leading zeros
+        while (leadingZeros > 0) {
+            from.remove(from.size() - 1);
+            leadingZeros--;
         }
     }
 
