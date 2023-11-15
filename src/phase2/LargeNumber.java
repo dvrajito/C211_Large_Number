2c3571159b6d2005fff8f221265d0f369e3affcd
charliekinnett <147398957+charliekinnett@users.noreply.github.com>
2023-11-15 10:24:09 +0500

    Update LargeNumber.java
    Added class Board

--- src/phase2/LargeNumber.java
+++ src/phase2/LargeNumber.java
@@ -501,3 +501,49 @@ public class LargeNumber implements Comparable<LargeNumber> {
         // }
     }
 }
+
+
+
+//phase 3 with a new class named as Board
+//Charlie Kinnett
+//FA23-SB-INFO-C211-19523
+
+public class Board {
+    private LargeNumberHelper[][] board;
+    private int rows;
+    private int cols;
+
+    public Board(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        initializeBoard();
+    }
+
+
+    public LargeNumberHelper getValueAt(int row, int col) {
+        return board[row][col];
+    }
+
+    public void setValueAt(int row, int col, LargeNumberHelper value) {
+        board[row][col] = value;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public void setCols(int cols) {
+        this.cols = cols;
+    }
+
+   
+}
+
